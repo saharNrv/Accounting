1e2a6bfb8a78988fc500698f228bd5dcea1ca223
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-07-16 12:27:07 +0330

    .

--- src/app/page.js
+++ src/app/page.js
@@ -3,37 +3,52 @@ import style from '@/styles/Home.module.css'
 import { FaUser } from 'react-icons/fa';
 import { IoSettings } from "react-icons/io5";
 import { FaChartPie } from "react-icons/fa6";
+import Link from 'next/link';
+import Box from '@/components/module/box/Box';
 
 export default function page() {
     return (
-        <div className={style.homeWrapper}>
-            {/* home navbar */}
-            <div className={style.homeNav}>
-                {/* home navbar right */}
-                <div className={style.homeNavRight}>
-                    <div className={style.homeNavIconWrap}>
-                        <FaUser className={style.homeNavIcon} />
+        <>
+            <div className={style.homeWrapper}>
+                {/* home navbar */}
+                <div className={style.homeNav}>
+                    {/* home navbar right */}
+                    <div className={style.homeNavRight}>
+                        <div className={style.homeNavIconWrap}>
+                            <FaUser className={style.homeNavIcon} />
+                        </div>
+                        <div className={style.homeNavInfo}>
+                            <p className={style.homeNavName}>سحر نوری</p>
+                            <p className={style.homeNavPhone}>09124568956</p>
+                        </div>
                     </div>
-                    <div className={style.homeNavInfo}>
-                        <p className={style.homeNavName}>سحر نوری</p>
-                        <p className={style.homeNavPhone}>09124568956</p>
+                    {/* home navbar left */}
+                    <div className={style.homeNavLeft}>
+                        <div className={style.homeNavLeftIconWrap}>
+                            <FaChartPie />
+                            <IoSettings />
+
+                        </div>
                     </div>
                 </div>
-                {/* home navbar left */}
-                <div className={style.homeNavLeft}>
-                    <div className={style.homeNavLeftIconWrap}>
-                        <FaChartPie />
-                        <IoSettings />
-
-                    </div>
-                </div>
-
-
-            </div>
+                {/* home navbar price */}
                 <div className={style.homeNavPricce}>
                     <h1>20000 تومان</h1>
                 </div>
 
-        </div>
+            </div>
+            {/* recent expenses */}
+            <div className={style.recentExpenses}>
+                <h3>مخارج اخیر</h3>
+                <Link href={'/carts'}>مشاهده همه</Link>
+            </div>
+            <div className={style.homeBox}>
+                <Box />
+                <Box />
+
+            </div>
+        </>
+
+
     );
 }
