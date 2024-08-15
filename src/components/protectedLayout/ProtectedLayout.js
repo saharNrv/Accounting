a1c06125f8b44096cdbbae1034957aca53596f6a
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-08-15 21:24:43 +0330

    .

--- src/components/protectedLayout/ProtectedLayout.js
+++ src/components/protectedLayout/ProtectedLayout.js
@@ -1,28 +1,65 @@
-"use client"
-import { usePathname, useRouter } from 'next/navigation';
-import React, { useContext, useEffect } from 'react';
-import { AuthorizationProviderContext } from '../../../context/AuthorizationProvider';
+// "use client"
+// import { usePathname, useRouter } from 'next/navigation';
+// import React, { useContext, useEffect } from 'react';
+// import { AuthorizationProviderContext } from '../../../context/AuthorizationProvider';
+
+// export default function ProtectedLayout({ children }) {
+
+//   const router = useRouter()
+//   const path = usePathname()
+//   const { isAuthenticate } = useContext(AuthorizationProviderContext)
+
+//   useEffect(() => {
+//     if (isAuthenticate() && ['/login-register'].includes(path)) {
+//       router.replace('/')
+//     }
+//     if (!isAuthenticate() && ['/login-register'].includes(path) === false) {
+//       router.replace('/login-register')
+//     }
+//   }, [])
+
+//   return (
+//     <>
+//       {children}
+//     </>
+//   );
+// }
+
+// // export default dynamic(() => Promise.resolve(ProtectedLayout), { ssr: false })
+
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthorizationProviderContext } from "../../../context/AuthorizationProvider";
 
 export default function ProtectedLayout({ children }) {
-
-  const router = useRouter()
-  const path = usePathname()
-  const { isAuthenticate } = useContext(AuthorizationProviderContext)
+  const router = useRouter();
+  const path = usePathname();
+  const { isAuthenticate } = useContext(AuthorizationProviderContext);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (isAuthenticate() && ['/login-register'].includes(path)) {
-      router.replace('/')
-    }
-    if (!isAuthenticate() && ['/login-register'].includes(path) === false) {
-      router.replace('/login-register')
-    }
-  }, [])
+    const authenticate = async () => {
+      const authenticated = isAuthenticate();
 
-  return (
-    <>
-      {children}
-    </>
-  );
+      if (authenticated && ["/login-register"].includes(path)) {
+        router.replace("/");
+      } else if (
+        !authenticated &&
+        ["/login-register"].includes(path) === false
+      ) {
+        router.replace("/login-register");
+      } else {
+        setLoading(false);
+      }
+    };
+
+    authenticate();
+  }, [path]);
+
+  if (loading) {
+    return null; // or a loading spinner, or a placeholder component
+  }
+
+  return <>{children}</>;
 }
-
-// export default dynamic(() => Promise.resolve(ProtectedLayout), { ssr: false })
