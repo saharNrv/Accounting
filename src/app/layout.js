819563ab05d7fc9d18827938830998f110d51194
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-08-15 18:44:12 +0330

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,37 +1,50 @@
-"use client"
-import { usePathname, useRouter } from 'next/navigation';
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthorizationProviderContext } from '../../context/AuthorizationProvider';
+import "./globals.css";
+import { CultureProvider } from "../../context/CultureProvider";
+import ClientOnly from "@/components/client_only/ClientOnly";
+import { AuthorizationProvider } from "../../context/AuthorizationProvider";
 
-export default function ProtectedLayout({ children }) {
-  const router = useRouter();
-  const path = usePathname();
-  const { isAuthenticate } = useContext(AuthorizationProviderContext);
-  const [loading, setLoading] = useState(true);
+export const metadata = {
+  title: "Hesabeto",
+  description: "Your accounting software",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["Hesabeto", "Accountin Application"],
+  icons: [
+    { rel: "apple-touch-icon", url: "icon512x512.png" },
+    { rel: "icon", url: "icon512x512.png" },
+  ],
+};
 
-  useEffect(() => {
-    const authenticate = async () => {
-      const authenticated = isAuthenticate();
+// Export themeColor separately
+export const themeColor = [
+  { media: "(prefers-color-scheme: dark)", color: "#fff" },
+];
 
-      if (authenticated && ['/login-register'].includes(path)) {
-        router.replace('/');
-      } else if (!authenticated && ['/login-register'].includes(path) === false) {
-        router.replace('/login-register');
-      } else {
-        setLoading(false);
-      }
-    };
-
-    authenticate();
-  }, [path]);
-
-  if (loading) {
-    return null; // or a loading spinner, or a placeholder component
-  }
-
+// Export viewport separately
+export const viewport = {
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+};
+export default function RootLayout({ children }) {
   return (
-    <>
-      {children}
-    </>
+    <html lang="fa">
+      <head>
+        <title>Hesabeto</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="generator" content={metadata.generator} />
+        <link rel="manifest" href={metadata.manifest} />
+        <meta name="keywords" content={metadata.keywords.join(", ")} />
+        {metadata.icons.map(({ rel, url }, index) => (
+          <link key={index} rel={rel} href={url} />
+        ))}
+      </head>
+      <body>
+        <ClientOnly>
+          <AuthorizationProvider>
+            <CultureProvider>{children}</CultureProvider>
+          </AuthorizationProvider>
+        </ClientOnly>
+      </body>
+    </html>
   );
 }
